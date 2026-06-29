685ff15458f3848b1f387d6d5e904e7e0c1a7807
Daria Yakovenko <darrriax@gmail.com>
2026-06-29 21:33:49 +0300

    Replaced images, changed description of StoredProcedure: silver.load_silver

--- SQL-Data-Warehouse-Project/Scripts/Silver/load_data_silver.sql
+++ SQL-Data-Warehouse-Project/Scripts/Silver/load_data_silver.sql
@@ -3,10 +3,11 @@
 Stored Procedure: Load Silver Layer (Bronze -> Silver)
 
 Script Purpose:
-    This stored procedure loads data into the 'silver' schema from the 'bronze' schema tables.
-    It performs the following actions:
-    - Truncates the silver tables before loading data.
-    - Uses the 'FROM' command to load data from bronze tables to silver tables.
+    This stored procedure performs the ETL (Extract, Transform, Load) process 
+    to populate the 'silver' schema tables from the 'bronze' schema.
+    Actions Performed:
+    - Truncates Silver tables.
+    - Inserts transformed and cleansed data from Bronze into Silver tables.
 Parameters:
     None.
     This stored procedure does not accept any parameters or return any values.
